7dd1a0e4586b1e6d9a2b1cbbe3197beb6522f429
hyorimm <hyorimm@gmail.com>
2023-01-23 00:20:23 +0900

    #13 - feat: 주문내역 마크업 진행 중

--- src/pages/MyPage/MyOrder.tsx
+++ src/pages/MyPage/MyOrder.tsx
@@ -5,63 +5,76 @@ const MyOrder = () => {
     <Container>
       <h2>주문 내역</h2>
       <ItemContainer>
-        <ItemImageWrapper>
+        <h3>결제 완료</h3>
+        <ItemInfoWrapper>
           <img
             src="https://w.namu.la/s/ce183abef5e87594e9b0c182844ef87ad1679448ae06239ae8e4db1004c577a9dd67d3048532f18f13937fe4ef96e487abb8166b7afa7d4c07ff9eb8cbec249ce620af833f563f63dacfd0fd04228027"
             alt="상품이미지"
           />
-        </ItemImageWrapper>
-        <ItemInfoContainer>
-          <p>상품명</p>
-          <span>상품 가격</span>
-          <OrderDate> | 상품 구매 일자</OrderDate>
-          <ConfirmOrderContainer>
-            <Cfmbtn type="button">구매확정</Cfmbtn>
-            <Cfmbtn type="button">구매취소</Cfmbtn>
-          </ConfirmOrderContainer>
-        </ItemInfoContainer>
+          <ItemInfoContainer>
+            <p>2021.12.24 구매</p>
+            <span>도라에몽 인형</span>
+            <span>5,000원</span>
+          </ItemInfoContainer>
+        </ItemInfoWrapper>
+        <ConfirmOrderContainer>
+          <Cfmbtn type="button">구매확정</Cfmbtn>
+          <Cfmbtn type="button">구매취소</Cfmbtn>
+        </ConfirmOrderContainer>
       </ItemContainer>
     </Container>
   );
 };
 export default MyOrder;
 
-const Container = styled.div`
+const Container = styled.ul`
+  max-width: 750px;
   padding: 1.875rem 1rem;
   margin: 1.875rem auto 0;
-  max-width: 450px;
   h2 {
     margin-bottom: 0.875rem;
+    margin-left: 150px;
   }
 `;
 
-const ItemContainer = styled.div`
-  width: 680px !important;
-  margin-inline
-  border: 1px solid var(--black-20);
-  display: flex;
-  padding: 10px;
+const ItemContainer = styled.li`
+  /* border: 1px solid var(--black-20); */
+  box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.08), 1px 3px 3px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding: 20px;
 `;
 
-const ItemImageWrapper = styled.div`
+const ItemInfoWrapper = styled.div`
+  display: flex;
+  padding-top: 10px;
   img {
-    max-width: 150px;
+    max-width: 100px;
   }
 `;
 
 const ItemInfoContainer = styled.div`
   display: inline-block;
-  padding: 10px;
+  margin-left: 20px;
+  p {
+    color: var(--black-30);
+    font-size: 14px;
+    line-height: 2;
+  }
+  span {
+    display: block;
+  }
 `;
-
-const OrderDate = styled.span``;
 
 const ConfirmOrderContainer = styled.div`
   display: flex;
+  justify-content: center;
   gap: 10px;
-  margin-top: 20px;
+  margin-top: 30px;
 `;
 
 const Cfmbtn = styled.button`
-  border: 1px solid var(--primary-color);
+  border: 1px solid var(--black-20);
+  width: 280px;
+  border-radius: 5px;
+  padding: 5px 0;
 `;
