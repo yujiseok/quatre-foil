c46f474aefa54ba036d7ced0d57952dd07965f62
hyorimm <hyorimm@gmail.com>
2023-03-03 14:52:30 +0900

    #49 - feat: 구매 확정, 취소 함수 작성

--- src/api/product.ts
+++ src/api/product.ts
@@ -11,3 +11,23 @@ export const purchase = async (productId: string, accountId: string) => {
     },
   });
 };
+
+// 제품 구매 확정
+export const cfmPurchase = async (detailId: string) => {
+  return client("/products/ok", {
+    method: "POST",
+    data: {
+      detailId,
+    },
+  });
+};
+
+// 제품 구매 취소
+export const cancelPurchase = async (detailId: string) => {
+  return client("/products/cancel", {
+    method: "POST",
+    data: {
+      detailId,
+    },
+  });
+};
