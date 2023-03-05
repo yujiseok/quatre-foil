ea2ed3b71a462a4682cac0d2deba8af71c956d1b
yujiseok <koesijuy@gmail.com>
2023-03-05 22:28:12 +0900

    #51 - feat: 메인, 상세페이지 스타일 및 상세 이미지 추가

--- src/api/product.ts
+++ src/api/product.ts
@@ -10,6 +10,18 @@ interface Product {
   thumbnail: string; // 제품 썸네일 이미지(URL)
   isSoldOut: boolean; // 제품 매진 여부
 }
+
+export interface IProductDetail {
+  // 제품의 상세 내용
+  id: string; // 제품 ID
+  title: string; // 제품 이름
+  price: number; // 제품 가격
+  description: string; // 제품 상세 설명
+  tags: string[]; // 제품 태그
+  thumbnail: string; // 제품 썸네일 이미지(URL)
+  photo: string; // 제품 상세 이미지(URL)
+  isSoldOut: boolean; // 제품 매진 여부
+}
 export const getAllProducts = async () => {
   const { data } = await client<Product[]>({
     url: "products",
@@ -22,7 +34,7 @@ export const getAllProducts = async () => {
 };
 
 export const getProduct = async (id: string) => {
-  const { data } = await client.get<Product>(`/products/${id}`);
+  const { data } = await client.get<IProductDetail>(`/products/${id}`);
 
   return data;
 };
