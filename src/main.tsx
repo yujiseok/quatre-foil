277f2e662cd8ac70c8357320a776d9478c77bc97
yujiseok <koesijuy@gmail.com>
2023-03-05 15:22:20 +0900

    feat: 리액트 쿼리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,22 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { persistor, store } from "app/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { GlobalStyles } from "./global/globalStyles";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <QueryClientProvider client={queryClient}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
     <GlobalStyles />
   </>,
