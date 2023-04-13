0e90395914a6c03c5d129092de1ce301a81b2367
yujiseok <koesijuy@gmail.com>
2023-04-13 14:19:36 +0900

    feat: 상품 상세 더미 정보 컴포넌트 추가

---
+++ src/components/ProductNotice.tsx
@@ -0,0 +1,112 @@
+const ProductNotice = () => {
+  return (
+    <div className="desc-footer">
+      <p>&nbsp;</p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>배송안내</strong>
+        </span>
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>배송 업체 ㅣ&nbsp;</strong>CJ 대한통운 (1588-1255)
+        </span>
+      </p>
+
+      <p>
+        <span>
+          <strong>배송 지역 ㅣ&nbsp;</strong>대한민국 전지역
+        </span>
+      </p>
+
+      <p>
+        <span>
+          <strong>배송 기간 ㅣ&nbsp;</strong>주말·공휴일 제외 2일 ~ 5일
+        </span>
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>유의 사항</strong>
+        </span>
+      </p>
+
+      <p>
+        <span>
+          -&nbsp;주문폭주 및 공급 사정으로 인하여 지연 및 품절이 발생될 수
+          있습니다.&nbsp;
+        </span>
+      </p>
+
+      <p>
+        <span>
+          - 기본 배송기간 이상 소요되는 상품이거나, 품절 상품은 개별 연락을
+          드립니다.
+        </span>
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>교환 및 반품안내</strong>
+        </span>
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>신청 방법 ㅣ&nbsp;</strong>상품을 수령하신 날로부터 7일 이내로
+          콜센터(1234-1234)&nbsp;및 홈페이지 Q&amp;A 게시판을 통해&nbsp;접수
+        </span>
+      </p>
+
+      <p>
+        <span>
+          <strong>반품 주소 ㅣ&nbsp;</strong>서울특별시 서초구 강남대로
+        </span>
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>유의 사항</strong>
+        </span>
+      </p>
+
+      <p>
+        <span>
+          - 단순 변심의 경우 수령일로부터 7일 이내까지&nbsp;교환∙반품이
+          가능합니다. (교환/반품비 고객님 부담)
+        </span>
+      </p>
+
+      <p>
+        <span>
+          - 상품 하자, 오배송의 경우 수령일로부터 7일 이내 고객센터 접수
+          후&nbsp;교환∙반품이 가능합니다. (교환/반품비 무료)
+        </span>
+      </p>
+
+      <p>
+        <span>
+          - 제품 특성상 단순 변심, 부주의에 의한 제품 손상 및 파손, 사용 및
+          개봉한 경우 교환/반품이 불가합니다.
+        </span>
+      </p>
+    </div>
+  );
+};
+export default ProductNotice;
