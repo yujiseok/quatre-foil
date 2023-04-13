a08913872577a50b70c3866523db9e966134ab41
yujiseok <koesijuy@gmail.com>
2023-04-13 14:11:42 +0900

    refactor: `BrowserRouter` -> `createBrowserRouter`
    - Suspense 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import Layout from "@components/Layout";
-import ScrollToTop from "@components/Layout/ScrollToTop";
 import PrivateRouter from "@components/PrivateRouter";
 import PublicRouter from "@components/PublicRouter";
+import Spinner from "@components/Spinner";
 import Cart from "@pages/Cart";
 import Detail from "@pages/Detail";
 import Home from "@pages/Home";
@@ -13,50 +13,132 @@ import MyOrder from "@pages/MyPage/MyOrder";
 import Purchase from "@pages/Purchase";
 import Shop from "@pages/Shop";
 import Signup from "@pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:productId" element={<Detail />} />
-          <Route
-            path="/login"
-            element={
-              <PublicRouter>
-                <Login />
-              </PublicRouter>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <PublicRouter>
-                <Signup />
-              </PublicRouter>
-            }
-          />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/purchase/:productId" element={<Purchase />} />
-          <Route
-            path="/mypage"
-            element={
-              <PrivateRouter>
-                <Mypage />
-              </PrivateRouter>
-            }
-          >
-            <Route path="order" element={<MyOrder />} />
-            <Route path="account" element={<MyAccount />} />
-            <Route path="mypage" element={<MyInfo />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route
+        index
+        path="/"
+        element={
+          <Suspense fallback={<Spinner />}>
+            <Home />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/shop"
+        element={
+          <Suspense fallback={<Spinner />}>
+            <Shop />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/shop/:productId"
+        element={
+          <Suspense fallback={<Spinner />}>
+            <Detail />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicRouter>
+            <Login />
+          </PublicRouter>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <PublicRouter>
+            <Signup />
+          </PublicRouter>
+        }
+      />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/purchase/:productId" element={<Purchase />} />
+      <Route
+        path="/mypage"
+        element={
+          <PrivateRouter>
+            <Mypage />
+          </PrivateRouter>
+        }
+      >
+        <Route path="order" element={<MyOrder />} />
+        <Route path="account" element={<MyAccount />} />
+        <Route path="mypage" element={<MyInfo />} />
+      </Route>
+    </Route>,
+  ),
+);
+
+// const Router = () => {
+//   return (
+//     <BrowserRouter>
+//       {/* <ScrollToTop /> */}
+//       <Routes>
+//         <Route element={<Layout />}>
+//           <Route
+//             index
+//             path="/"
+//             element={
+//               <Suspense fallback={<Spinner />}>
+//                 <Home />
+//               </Suspense>
+//             }
+//           />
+//           <Route path="/shop" element={<Shop />} />
+//           <Route
+//             path="/shop/:productId"
+//             element={
+//               <Suspense fallback={<Spinner />}>
+//                 <Detail />
+//               </Suspense>
+//             }
+//           />
+//           <Route
+//             path="/login"
+//             element={
+//               <PublicRouter>
+//                 <Login />
+//               </PublicRouter>
+//             }
+//           />
+//           <Route
+//             path="/signup"
+//             element={
+//               <PublicRouter>
+//                 <Signup />
+//               </PublicRouter>
+//             }
+//           />
+//           <Route path="/cart" element={<Cart />} />
+//           <Route path="/purchase/:productId" element={<Purchase />} />
+//           <Route
+//             path="/mypage"
+//             element={
+//               <PrivateRouter>
+//                 <Mypage />
+//               </PrivateRouter>
+//             }
+//           >
+//             <Route path="order" element={<MyOrder />} />
+//             <Route path="account" element={<MyAccount />} />
+//             <Route path="mypage" element={<MyInfo />} />
+//           </Route>
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
 export default Router;
