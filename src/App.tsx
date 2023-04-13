e9d314e030ef827312052893ce82c543f69f5490
yujiseok <koesijuy@gmail.com>
2023-04-13 14:12:03 +0900

    refactor: `RouterProvider`로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { RouterProvider } from "react-router-dom";
 import Router from "Router";
 
 const App = () => {
-  return <Router />;
+  return <RouterProvider router={Router} />;
 };
 export default App;
