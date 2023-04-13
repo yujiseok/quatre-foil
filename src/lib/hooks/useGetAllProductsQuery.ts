99e67436b7598ece8b41f46e80629bfb9bcf381b
yujiseok <koesijuy@gmail.com>
2023-04-13 17:09:20 +0900

    refactor: `category` 없을 시 분기 처리

--- src/lib/hooks/useGetAllProductsQuery.ts
+++ src/lib/hooks/useGetAllProductsQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getAllProducts } from "api/product";
 import type { Category } from "features/categorySlice";
 
-const useGetAllProductsQuery = (category: Category) => {
+const useGetAllProductsQuery = (category?: Category) => {
   const {
     data: products,
     isLoading,
@@ -10,14 +10,16 @@ const useGetAllProductsQuery = (category: Category) => {
   } = useQuery({
     queryKey: ["products"],
     queryFn: getAllProducts,
-    select: (data) =>
-      data?.filter((product) =>
-        product.tags.find((tag) => {
-          if (tag === category.toLowerCase() || category === "ALL") {
-            return product;
-          }
-        }),
-      ),
+    select: category
+      ? (data) =>
+          data?.filter((product) =>
+            product.tags.find((tag) => {
+              if (tag === category.toLowerCase() || category === "ALL") {
+                return product;
+              }
+            }),
+          )
+      : undefined,
   });
 
   return { products, isLoading, isFetching };
